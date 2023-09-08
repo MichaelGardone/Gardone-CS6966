eef5c97e62dc5dc09d5ae6b17283edb5c21f2eae
MichaelGardone <michaelgardone@gmail.com>
2023-09-07 22:57:11 -0600

    initial commit for asg2

---
+++ Assignment2/assignment_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --account soc-gpu-np
+#SBATCH --partition soc-gpu-np
+#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a100:1
+#SBATCH --time=8:00:00
+#SBATCH --mem=40GB
+#SBATCH --mail-user=u1000771@umail.utah.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH -o assignment_2-%j
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate cs6966
+
+mkdir -p /scratch/general/vast/u1000771/huggingface_cache
+export TRANSFORMER_CACHE="/scratch/general/vast/u1000771/huggingface_cache"
+
+OUT_DIR=/scratch/general/vast/u1000771/cs6966/assignment1/models
+mkdir -p ${OUT_DIR}
+python assignment2.py --output_dir ${OUT_DIR}
